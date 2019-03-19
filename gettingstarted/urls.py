2253e6dceb90590d755d07d19e4e9f23b7c66190
Rokas Gulbinas <rkgulbinas@gmail.com>
2019-03-19 18:15:25 +0200

    Add post submission form

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     # ex: .../penktas postas/
     path('<int:post_id>/', hello.views.detail, name='detail'),
+    path("publish/", hello.views.publishPost, name='publish'),
     path("accounts/", include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
     path("accounts/login/", include('django.contrib.auth.urls')),
--- hello/models.py
+++ hello/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.forms import ModelForm
 
 # Create your models here.
 class Greeting(models.Model):
@@ -23,3 +24,8 @@ class Review(models.Model):
     publish_date = models.DateField("Published on", auto_now_add=True, null=True)
     def __str__(self):
         return str(self.id)  + " " + self.headline
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['headline', 'price', 'content']
